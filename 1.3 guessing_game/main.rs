a1c1c51acd230fa7edeb681743b9a8d6f78ce8f8
Isaac Cespedes <isaac.cespedes1@gmail.com>
2023-07-19 02:51:57 -0400

    ending chapter 9

--- 1.3 guessing_game/main.rs
+++ 1.3 guessing_game/main.rs
@@ -2,6 +2,25 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+// error handling via types
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -15,14 +34,20 @@ fn main() {
             .read_line(&mut guess) // returns io::Result (Ok or Err)
             .expect("Failed to read line"); // unwraps ok, panics / crashes on Err
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        // let guess: u32 = match guess.trim().parse() {
+        //     Ok(num) => num,
+        //     Err(_) => continue,
+        // };
+
+        // instead of a u32, we can use a custom type to handle errors
+        let guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.value);
 
-        match guess.cmp(&secret_number) {
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
--- 8 error_handling/src/main.rs
+++ 8 error_handling/src/main.rs
@@ -1,7 +1,12 @@
+use std::fs::File;
 use std::io::ErrorKind;
+use std::io::{self, Read};
 
+// main can return a Result<T, E> as well
+// Result<(), Box<dyn Error>> is a common return type for main
+// main can return any type that implements the Termination trait
 fn main() {
-    // panics default to unwinding and cleaning up data first
+    // panics (for unrecoverable errors) default to unwinding and cleaning up data first
     // if you want to immediately abort, use panic = 'abort' in Cargo.toml
     // the OS will then have to clean up memory, but this keeps the binary smaller
 
@@ -20,6 +25,7 @@ fn main() {
 
     let greeting_file_result = File::open("hello.txt");
 
+    // handling files with match
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -32,4 +38,26 @@ fn main() {
             }
         },
     };
+
+    // handling files with closures and unwrap_or_else
+    let greeting_file2 = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+// the ? operator returns a value or errors, coverting to the calling function's return type
+// it can only be used in functions that return Result or Option
+// similar to Result,  with Options, if the value is None, the None will be returned early from the function at that point.
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
 }
